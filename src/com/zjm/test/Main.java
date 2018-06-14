fdfadfde864aa842e5a8d2fb3a36e7d9539de1cd
folowyoarehater <573467164@qq.com>
2018-06-14 15:03:05 +0800

    提交修改

--- src/com/zjm/test/Main.java
+++ src/com/zjm/test/Main.java
@@ -2,8 +2,7 @@ package com.zjm.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hahahha");
         Shanghao shanghao = new Shanghao();
-        shanghao.readFileToJsonArrayFile(Common.desktop + "100.txt", 50, 2);
+        shanghao.readFileToJsonArrayFile(Common.desktop + "tggg253.txt", 100, 2);
     }
 }
